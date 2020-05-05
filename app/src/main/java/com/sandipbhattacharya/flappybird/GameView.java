753c5bddf52a20c399db94057300bef2c1260042
unknown <arturo.caronongan@delasalle.ph>
2020-05-05 18:38:22 +0800

    Initial commit

---
+++ app/src/main/java/com/sandipbhattacharya/flappybird/GameView.java
@@ -0,0 +1,68 @@
+package com.sandipbhattacharya.flappybird;
+
+import android.content.Context;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class GameView extends SurfaceView implements SurfaceHolder.Callback{
+
+    GameThread gameThread;
+
+    public GameView(Context context) {
+        super(context);
+        initView();
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        if(!gameThread.isRunning()){
+            gameThread = new GameThread(holder);
+            gameThread.start();
+        }else{
+            gameThread.start();
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        if(gameThread.isRunning()){
+            gameThread.setIsRunning(false);
+            boolean retry = true;
+            while(retry){
+                try{
+                    gameThread.join();
+                    retry = false;
+                }catch(InterruptedException e){}
+            }
+        }
+    }
+
+    void initView(){
+        SurfaceHolder holder = getHolder();
+        holder.addCallback(this);
+        setFocusable(true);
+        gameThread = new GameThread(holder);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int action = event.getAction();
+        // Tap is detected
+        if(action == MotionEvent.ACTION_DOWN){
+            if(AppConstants.getGameEngine().gameState == 0){
+                AppConstants.getGameEngine().gameState = 1;
+                AppConstants.getSoundBank().playSwoosh();
+            }else{
+                AppConstants.getSoundBank().playWing();
+            }
+            AppConstants.getGameEngine().bird.setVelocity(AppConstants.VELOCITY_WHEN_JUMPED);
+        }
+        return true;
+    }
+}
